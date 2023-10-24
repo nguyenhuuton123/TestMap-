ab5f2bb5d3ecf4c44287ebf51811d03a052bbdee
nguyenhuuton123 <126746633+nguyenhuuton123@users.noreply.github.com>
2023-10-24 11:08:35 +0700

    Add files via upload

---
+++ TestMap/src/TestMap.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class TestMap  {
+    public static void main(String[] args) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Smith", 30);
+        hashMap.put("Anderson", 31);
+        hashMap.put("Lewis", 29);
+        hashMap.put("Cook", 29);
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashMap + "\n");
+        }
+    }
